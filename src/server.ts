55134367f0d559f067a40ac10b72e0ba5a8319eb
Guilherme <guisinatora@hotmail.com>
2021-02-23 22:10:46 -0300

    Fim da segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from './routes';
 
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3333, () => console.log("Server is UP!"));
 
 /**
  * GET => Buscar
@@ -11,19 +18,7 @@ const app = express();
  * PUT => Alterar
  * DELETE => Deletar
  * PATCH => Alteração específica
- * 
+ *
  * 1 param => Rota
  * 2 param => request, response
  */
-
-app.get("/", (request, response) => {
-  
-  return response.json({message: "Hello World - GET"})
-});
-
-app.post("/", (request, response) =>{
-
-  return response.json({message: "Hello World - POST"})
-})
-
-app.listen(3333, () => console.log("Server up!"));
